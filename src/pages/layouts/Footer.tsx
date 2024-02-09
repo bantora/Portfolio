e0a2471f4dd83e850516ad33232db66cfd99ac2b
Ryan Paolo Joson <bantora.1011@gmail.com>
2024-02-09 16:49:33 +0800

    chore: footer style

--- src/pages/layouts/Footer.tsx
+++ src/pages/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import type { ReactElement } from 'react'
 const Footer = (): ReactElement => {
 	return (
 		<div className='flex justify-center'>
-			<a href='https://github.com/bantora/Portfolio' target='_blank'>Design inspired by Brittany Chang and built by Ryan Paolo Joson.</a >
+			<a href='https://github.com/bantora/Portfolio' className='mb-8 text-teal-400' target='_blank'>Design inspired by Brittany Chang and built by Ryan Paolo Joson.</a >
 		</div >
 	)
 }
